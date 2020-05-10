3a0ad9bb9c7d31474c36d0fa135cba647e92e2fb
karintomania <19652340+karintomania@users.noreply.github.com>
2020-05-10 13:58:01 +0100

    add quiz function

---
+++ app/src/main/java/com/example/regexquest/quiz/Quiz.kt
@@ -0,0 +1,7 @@
+package com.example.regexquest.quiz
+
+data class Quiz (
+    val quiz: String,
+    val match: String,
+    val answers: MutableList<String>
+)
--- app/src/main/java/com/example/regexquest/quiz/QuizViewModel.kt
+++ app/src/main/java/com/example/regexquest/quiz/QuizViewModel.kt
@@ -1,20 +1,59 @@
 package com.example.regexquest.quiz
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class QuizViewModel : ViewModel() {
+
+    private val tag = "QuizViewModel"
+
+    private val quizAmount = 3;
+    private var currentQuizIndex = 0
+
+
     private val _navigateToResult = MutableLiveData<Boolean?>()
     val navigateToResult: LiveData<Boolean?>
         get() = _navigateToResult
 
-    fun onAnswer(){
-        _navigateToResult.value = true
+    private val quizzes: MutableList<Quiz?> = mutableListOf(
+        Quiz(quiz = "foo",match = "foo",answers = mutableListOf("answer","b","c","d")),
+        Quiz(quiz = "bar",match = "bar",answers = mutableListOf("answer","b","c","d")),
+        Quiz(quiz = "foobar",match = "foobar",answers = mutableListOf("answer","b","c","d"))
+    )
+
+
+    private val _currentQuiz = MutableLiveData<Quiz?>()
+    val currentQuiz: LiveData<Quiz?>
+        get() = _currentQuiz
+
+    init{
+        setCurrentQuiz()
+    }
+
+    fun onAnswer(index: Int){
+        if(quizzes[currentQuizIndex]!!.answers[0] == _currentQuiz.value!!.answers[index]){
+            currentQuizIndex ++
+
+            if(currentQuizIndex >= quizAmount){
+                _navigateToResult.value = true
+            }else{
+                setCurrentQuiz()
+            }
+        }
     }
 
     fun doneNavigate(){
         _navigateToResult.value = false
+    }
+
+    fun setCurrentQuiz(){
+        // deep copy of current quiz
+        val tmpQuiz = quizzes[currentQuizIndex]
+        _currentQuiz.value = Quiz(quiz = tmpQuiz!!.quiz, match = tmpQuiz!!.match, answers = tmpQuiz!!.answers.toMutableList())
+
+        _currentQuiz.value?.answers?.shuffle()
     }
 
 }
